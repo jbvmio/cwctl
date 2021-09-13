f550d7b19b79cdbd095148bc8b38e6e9203846c5
jbvmio <jbonds@jbvmio>
2021-09-13 17:45:20 -0400

    add computer conditions

--- cli/cmd/cmdGetComputers.go
+++ cli/cmd/cmdGetComputers.go
@@ -10,8 +10,34 @@ var cmdGetComputers = &cobra.Command{
 	Aliases: []string{"computer", "comps", "comp"},
 	Short:   "get computer details",
 	Run: func(cmd *cobra.Command, args []string) {
+		var condition, ids string
+		switch len(args) {
+		case 0:
+		case 1:
+			ids = `id eq ` + args[0]
+		default:
+			ids = `id in(` + args[0]
+			for _, id := range args[1:] {
+				ids += `, ` + id
+			}
+			ids += `)`
+		}
+		switch {
+		case !cmd.Flags().Changed(`client-id`):
+			if len(args) < 1 {
+				Failf("must use either --client-id and/or computer id(s) as arguments")
+			}
+			condition = ids
+		default:
+			switch len(args) {
+			case 0:
+				condition = `client.id eq ` + cliClientID
+			default:
+				condition = `(client.id eq ` + cliClientID + `) and ` + ids
+			}
+		}
+		cliFlags.Condition = condition
 		client := initClient(cfg)
-		cliFlags.Condition = `client.id eq ` + cliClientID
 		computers, err := cwctl.GetComputers(client, paramsComputer.merge(&cliFlags))
 		if err != nil {
 			Failf("error attempting GetComputers: %v", err)
@@ -24,5 +50,4 @@ func init() {
 	cmdGetComputers.Flags().StringVarP(&cliClientID, "client-id", `C`, "", "ID of the Client to target.")
 	cmdGetComputers.Flags().StringVarP(&cliFlags.Page, "page", `p`, cliFlags.Page, "Page number of results.")
 	cmdGetComputers.Flags().StringVarP(&cliFlags.PageSize, "page-size", `s`, cliFlags.PageSize, "Results per page.")
-	cmdGetComputers.MarkFlagRequired(`client-id`)
 }
